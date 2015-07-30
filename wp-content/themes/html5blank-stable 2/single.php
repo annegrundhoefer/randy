981c40a27805ef0d297e222419ffe3b1c7a7999a
Anne Grundhoefer <annegrundhoefer@gmail.com>
2015-07-30 12:37:24 -0500

    made changes to single.php

--- wp-content/themes/html5blank-stable 2/single.php
+++ wp-content/themes/html5blank-stable 2/single.php
@@ -1,67 +1,84 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
-	<section>
+<div class="clearfix"></div>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="page_title2">
+<div class="container">
+	
+    <h1><?php the_title(); ?></h1>
+</div>
+</div><!-- end page title --> 
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+<div class="clearfix"></div>
+
+<div class="content_fullwidth">
+	<div class="container">
+
+	<div class="content_left">
+        	
+        <div class="blog_post">	
+            <div class="blog_postcontent">
+            <div class="image_frame">
+            <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
 				</a>
 			<?php endif; ?>
-			<!-- /post thumbnail -->
+			</div>
+            <h3><?php the_title(); ?></h3>
+             <div class="clearfix"></div>
+             <div class="margin_top1"></div>
+            <p><?php the_content(); ?></p>
+            </div>
+        </div><!-- /# end post -->
+            
+            <div class="clearfix divider_line9 lessm"></div>
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+   <?php endwhile; ?>
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+		<?php else: ?>
 
-			<?php the_content(); // Dynamic Content ?>
+			<?php endif; ?>
+			</div>
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+			<div class="right_sidebar">
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+	<div class="sidebar_widget">
+    
+    	<div class="sidebar_title"><h4>Practice<i> Areas</i></h4></div>
+		<ul class="arrows_list1">		
+            <li><a href="#"><i class="fa fa-caret-right"></i> Economics</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Social Media</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Economics</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Online Gaming</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Entertainment</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Technology</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Make Money Online</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Photography</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Web Tutorials</a></li>
+		</ul>
+        
+	</div><!-- end section -->
+    
+    
+    
+    <div class="clearfix margin_top4"></div>
+    
+    <div class="clientsays_widget">
+    
+    	<div class="sidebar_title"><h4>Our <i>Clients</i></h4></div>
+        
+        <img src="http://placehold.it/50x50" alt="" />
+			<strong>- Michle Siminson</strong><p>Lorem Ipsum passage, and going through the cites of the word here classical literature passage discovere there undou btable source looks reasonable the generated charac eristic words.</p>  
+                
+	</div><!-- end section -->
+  </div>
+</div><!-- end right sidebar -->
+		</div>
+	</div>
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
